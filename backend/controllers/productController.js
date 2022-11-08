fbce45adea235ad7abe9833048cc067e0d9194e0
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-11-09 02:59:33 +0530

    Working on the backend and making sure CRUD could be performed.

---
+++ backend/controllers/productController.js
@@ -0,0 +1,68 @@
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
+
+//GET aLL PRODUCTS
+
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+//Get Product Details
+
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Update product -- Admin
+
+exports.updateProduct = async (req, res, next) => {
+  let product = Product.findById(req.params.id);
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Delete Product
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted Successfully",
+  });
+};
