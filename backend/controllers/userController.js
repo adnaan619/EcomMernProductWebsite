56d9b0a950705fca556b80eba972209f26820305
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-11-10 01:29:03 +0530

    Middleware authentication and route fixes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,8 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require('../utils/jwtToken')
 
+
+//Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     const { name, email, password } = req.body;
     
@@ -14,15 +16,8 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         },
     });
 
-    const token = user.getJWTToken();
-
-
-
-    res.status(201).json({
-        success: true,
-        token,
-    });
-})
+    sendToken(user, 201, res);
+});
 
 //Login User
 
@@ -47,5 +42,5 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
         return next(new ErrorHandler("Invalid email or password", 401))
     }
 
-   sendToken()
+    sendToken(user, 200, res);
 })
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -3,10 +3,10 @@
 const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
 
-    //options for cokiee
+    //options for cookie
     const options = {
         expires: new Date(
-            Date.now + process.env.COKIEE_EXPIRE * 24 * 60 * 60 * 1000
+            Date.now + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
     };
